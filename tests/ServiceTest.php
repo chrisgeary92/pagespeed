3b2e66e65308ff136e58337b70f878a08111f6f7
Christopher Geary <dev@crgeary.com>
2017-01-14 13:55:20 +0000

    Added initializable and url/argument merging PHP unit tests

---
+++ tests/ServiceTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Chrisgeary92\Pagespeed\Test;
+
+use Chrisgeary92\Pagespeed\Service;
+
+class ServiceTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function it_is_initializable()
+    {
+        $service = new Service();
+
+        $this->assertInstanceOf('Chrisgeary92\Pagespeed\Service', $service);
+    }
+
+    /** @test */
+    public function it_can_merge_url_with_optional_query_arguments()
+    {
+        $service = new Service();
+
+        $return = $service->getArguments('https://google.com', [
+            'strategy' => 'desktop',
+            'screenshot' => true
+        ]);
+
+        $this->assertArrayHasKey('url', $return);
+        $this->assertArrayHasKey('strategy', $return);
+        $this->assertArrayHasKey('screenshot', $return);
+    }
+}
